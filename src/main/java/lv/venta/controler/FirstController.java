2095c3cf0d9c7d46a41fd3ef8a4f36ab247bbe63
Lara Bernardes <contatolarabernardes@gmail.com>
2024-04-04 14:55:50 +0300

    add: hello msg page

--- src/main/java/lv/venta/controler/FirstController.java
+++ src/main/java/lv/venta/controler/FirstController.java
@@ -1,6 +1,7 @@
 package lv.venta.controler;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,5 +11,12 @@ public class FirstController {
 	public String getHello() {
 		System.out.println("First Controller!!!");
 		return "hello-page"; //tiek parādīta hello-page.html lapa
+	}
+	
+	@GetMapping("/hello/msg")//localhost:8080/hello/msg
+	public String getHelloMsg(Model model) {
+		System.out.println("Msg controller is called");
+		model.addAttribute("mydata", "Ziņa no JAVA Spring!!!");
+		return "hello-msg-page"; // tiek parādīra hello-msg-page.html lapa
 	}
 }
