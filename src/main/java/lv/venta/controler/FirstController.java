328e51fa4a88be043e6a6440d7f1634bd296ad21
Lara Bernardes <contatolarabernardes@gmail.com>
2024-04-04 15:39:58 +0300

    add:product

--- src/main/java/lv/venta/controler/FirstController.java
+++ src/main/java/lv/venta/controler/FirstController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lv.venta.model.Product;
+
 @Controller
 public class FirstController {
 	
@@ -18,5 +20,18 @@ public class FirstController {
 		System.out.println("Msg controller is called");
 		model.addAttribute("mydata", "Ziņa no JAVA Spring!!!");
 		return "hello-msg-page"; // tiek parādīra hello-msg-page.html lapa
+	}
+	
+	@GetMapping("/product/test")//localhost:8080/product/test
+	public String getHelloMsg(Model model) {
+		System.out.println("Msg controller is called");
+		model.addAttribute("mydata", "Ziņa no JAVA Spring!!!");
+		return "hello-msg-page"; // tiek parādīra hello-msg-page.html lapa
+	}
+	@GetMapping("/product/test")//localhost:8080/product/test
+	public String getProductTest(Model model) {
+		Product tempProduct = new Product("Abols", "Sarkans", 0.99f, 5);
+		model.addAttribute("mydata", tempProduct);
+		return "product-one-show-page";//tiek parādīta product-one-show-page.html lapa
 	}
 }
---
+++ src/main/java/lv/venta/model/Product.java
@@ -0,0 +1,35 @@
+package lv.venta.model;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+//@AllArgsConstructor
+public class Product {
+	@Setter(value = AccessLevel.NONE)// priekš ID nebūs automatiskais sets
+	private int id;
+	
+	private String title;
+	private String description;
+	private float price;
+	private int quantity;
+	
+	private static int counter = 1;
+	
+	public void setId() {
+		this.id = counter++;
+	}
+	public Product(String title, String description, float price, int quantity) {
+		setId();
+		setTitle(title);
+		setDescription(description);
+		setPrice(price);
+		setQuantity(quantity);
+	}
+}
