b073b75454804b1f707197fcb5099c0ec802de67
Lara Bernardes <contatolarabernardes@gmail.com>
2024-04-04 15:50:53 +0300

    add:all products

--- src/main/java/lv/venta/controler/FirstController.java
+++ src/main/java/lv/venta/controler/FirstController.java
@@ -1,5 +1,8 @@
 package lv.venta.controler;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,15 +26,18 @@ public class FirstController {
 	}
 	
 	@GetMapping("/product/test")//localhost:8080/product/test
-	public String getHelloMsg(Model model) {
-		System.out.println("Msg controller is called");
-		model.addAttribute("mydata", "Ziņa no JAVA Spring!!!");
-		return "hello-msg-page"; // tiek parādīra hello-msg-page.html lapa
-	}
-	@GetMapping("/product/test")//localhost:8080/product/test
 	public String getProductTest(Model model) {
 		Product tempProduct = new Product("Abols", "Sarkans", 0.99f, 5);
 		model.addAttribute("mydata", tempProduct);
 		return "product-one-show-page";//tiek parādīta product-one-show-page.html lapa
+	}
+	
+	@GetMapping("/product/test2")//localhost:8080/product/test2
+	public String getProductTest2(Model model) {
+		Product tempProduct = new Product("Abols", "Zilgan-zaļš", 0.99f, 5);
+		Product tempProduct2 = new Product("Bumbiers", "Dzeltens", 0.5f, 3);
+		ArrayList<Product> allproducts = new ArrayList<>(Arrays.asList(tempProduct,tempProduct2));
+		model.addAttribute("mydata", allproducts);
+		return "product-array-show-page";//tiek parādīta product-one-show-page.html lapa
 	}
 }
